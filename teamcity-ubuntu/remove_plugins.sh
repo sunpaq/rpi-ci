d34e1c4b635ff87b7984a46f11aa461a173cc69f
sunyuli <sunpaq@gmail.com>
2020-05-07 12:23:24 +0900

    update

---
+++ teamcity-ubuntu/remove_plugins.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+
+cd ./dist/TeamCity/webapps/ROOT/WEB-INF/plugins
+
+rm -rfv ant \
+ant-tool \
+Duplicator \
+server-side-inspections \
+ipr-runner \
+crash-detector \
+Maven2 \
+win32-distributor.zip \
+ant-net-tasks \
+bugzilla \
+cloud-amazon \
+coverage \
+dotNetRunners \
+dotnet-tools.zip \
+dotnet.cli.zip \
+eclipse-plugin-distributor.zip \
+golang.zip \
+gradle \
+idea-tool \
+intellij-plugin-distributor.zip \
+mercurial.zip \
+dotNetPackagesSupport.zip \
+perforce \
+powershell.zip \
+starteam \
+svn \
+s3-artifact-storage.zip \
+tfs \
+visualstudiotest \
+vs-addin-distributor.zip \
+cloud-vmware.zip \
+charisma 
